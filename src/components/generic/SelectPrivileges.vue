728fc51210eeb81c2033688b031c28bcf05beef0
akifo <akiho@bb07.net>
2017-03-27 21:19:02 +0900

    Add select privileges component

---
+++ src/components/generic/SelectPrivileges.vue
@@ -0,0 +1,34 @@
+<template>
+  <select v-model="selected" class="form-control">
+    <option
+      v-for="option in options"
+      :value="option.rank">
+      {{option.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+import PRIVILEGES from '../../../static/privileges.json'
+
+export default {
+
+  name: 'SelectLanguages',
+
+  data () {
+    return {
+      options: PRIVILEGES
+    }
+  },
+
+  props: [
+    'selected'
+  ],
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
