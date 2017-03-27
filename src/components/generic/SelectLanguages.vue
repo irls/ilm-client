2a34b633e15f37f0645eb5b46a70371a7be62ccf
akifo <akiho@bb07.net>
2017-03-27 21:18:16 +0900

    Add select languages component

---
+++ src/components/generic/SelectLanguages.vue
@@ -0,0 +1,35 @@
+<template>
+  <select v-model="selected" class="form-control">
+    <option disabled value="">Editor Languages</option>
+    <option
+      v-for="option in options"
+      :value="option.code">
+      {{option.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+import LANGUAGES from '../../../static/languages.json'
+
+export default {
+
+  name: 'SelectLanguages',
+
+  data () {
+    return {
+      options: LANGUAGES
+    }
+  },
+
+  props: [
+    'selected'
+  ],
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
