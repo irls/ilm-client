a67ee9cde7f33630305eb0861a59b29cbfa13f1f
akifo <akiho@bb07.net>
2017-03-27 17:27:15 +0900

    Change aslant rule "arrow-parents" 1 to "as-needed"

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,12 +18,12 @@ module.exports = {
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
-    'arrow-parens': 1,
+    'arrow-parens': ["error", "as-needed"],
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    
+
     // because some JS developers can be anal-retentive jackasses
     "no-trailing-spaces": 0,
     "no-multiple-empty-lines": 0,
