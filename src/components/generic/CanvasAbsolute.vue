c54c261ebfabf371c430425c0979fd53971f7837
Chad <chadananda@gmail.com>
2017-05-16 11:21:04 -0700

    added some missing files

---
+++ src/components/generic/CanvasAbsolute.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="canvase_absolute"></div>
+</template>
+
+
+
+<script>
+</script>
+
+
+
+
+
+
+<style>
+  #canvase_absolute {
+    display: none;
+    position: absolute;
+    top:0; left:0;
+    z-index:-1000;
+  }
+  .x2 {
+    z-index: 20000;
+    display: block;
+    transform: scale(2, 2);
+  }
+
+</style>
