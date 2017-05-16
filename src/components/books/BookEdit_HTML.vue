ebe98f419f0f1674c6f75914704cc0cf14c23060
Daniel Jones <djones1844@gmail.com>
2017-05-16 00:04:11 -0700

    sync html and json editor changes with global and db stores

--- src/components/books/BookEdit_HTML.vue
+++ src/components/books/BookEdit_HTML.vue
@@ -1,66 +1,96 @@
 <template>
   <div class='bookeditor'>
   <button id="save2" @click="savecontent" class=" button save fa fa-floppy-o"></button>  
-    <pre v-text="bookhtml" contenteditable="true" id="contentHTML"></pre>
+    <pre v-text="HTMLcontent" contenteditable="true" id="contentHTML"></pre>
     
   </div>
 </template>
 
 <script>
- import pretty from'pretty';
- import cheerio from 'cheerio';
+import Vue from 'vue'
+import access from "../../mixins/access.js"
+import PouchDB from 'pouchdb'
+import pretty from'pretty';
+import cheerio from 'cheerio';
 
  export default { 
-
   name: 'BookEditHtml',
-  
   data() { 
     return {
-      data: '',
-      componentState: 'start'
+      componentState: 'start',
+      HTMLcontent: "<b>No Content!</b>"
     }
   },
-  
+
+  mixins: [access],
+
   methods:{
-     savecontent: function(){
-      let data = [];
-      let t = document.getElementById('contentHTML').outerHTML;
-      t = t.replace(/&lt;/g, '<').replace(/&gt;/g, '>');
-      let c = cheerio.load(t);
+    savecontent: function(){
+      let vm = this
+      let data = this.html2json(document.getElementById('contentHTML').outerHTML)
+      let book = this.$store.state.currentBook
+      book.content = data    
+      try {
+        let db = this.libraryDB()
+        db.get(book._id).then(function(doc){
+          book._rev = doc._rev 
+          db.put(book).then(book => {
+            db.get(book.id).then(newbook => {
+              vm.$store.state.currentBook = JSON.parse(JSON.stringify(newbook))
+            })
+          }).catch(err => console.log(err))
+        })
+      }         
+      catch(e) { 
+        alert('An error has occurred: '+e.message)
+      }
+    },
+
+    // returns an array of objects, one per div
+    html2json: function(t){
+      let data = []
+     // let t = document.getElementById('contentHTML').outerHTML;
+      t = t.replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+      let c = cheerio.load(t)
       c('#book').find("div").each(function(i, el){
-        let item = {};
-        item.id = c(el).attr('id');
-        if(c(this).data('parnum')){ item.parnum = c(this).data('parnum');}
-        item.content = c(this).text();
-        item.classes = c(this).attr('class').split(' ');
-        data.push(item);
-      });
-      try { this.$store.state.currentBook.content = JSON.parse(JSON.stringify(data));}
-      catch(e) { alert('An error has occurred: '+e.message);}
-    }
-  },
+        let item = {}
+        item.id = c(el).attr('id')
+        if(c(this).data('parnum')){ 
+          item.parnum = c(this).data('parnum')
+        }
+        item.content = c(this).text()
+        item.classes = c(this).attr('class').split(' ')
+        data.push(item)
+      }); 
+      return data
+    },
 
-  components: {
-  },
-
-  computed: {
-    bookhtml: function() {
-      let book = this.$store.getters.currentBook;
-      let h = document.createElement('div');
-      h.setAttribute('ID', 'book');
+    // converts json object to formated html string
+    json2html: function(book){
+      let h = document.createElement('div')
+      h.setAttribute('ID', 'book')
       book.content.forEach( function(block){
-        let div = document.createElement('div');
-        let t = document.createTextNode(block.content);
-        div.appendChild(t);
-        div.setAttribute('id', block.id);
-        block.classes.forEach(function(c){div.className += c+' ';});
-        div.className = div.className.trim();
-        if(block.hasOwnProperty("parnum")){div.dataset.parnum = block.parnum;}
-        h.appendChild(div);
+        let div = document.createElement('div')
+        let t = document.createTextNode(block.content)
+        div.appendChild(t)
+        div.setAttribute('id', block.id)
+        block.classes.forEach( function(c){
+          div.className += c+' '
+        })
+        div.className = div.className.trim()
+        if(block.hasOwnProperty("parnum")){
+          div.dataset.parnum = block.parnum
+        }
+        h.appendChild(div)
       });
-      return pretty(h.outerHTML).replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+      return pretty(h.outerHTML).replace(/&lt;/g, '<').replace(/&gt;/g, '>')
     }
-  }  
+
+  },
+
+   mounted: function() {
+    this.HTMLcontent = this.json2html(this.$store.getters.currentBook)
+   }  
   
  }
 </script>
@@ -69,8 +99,8 @@
 <style scoped>
 pre {
     white-space: pre-wrap;
-    background-color: black;
-    color: white;
+    background-color: white;
+    color: black;
 }
  .save {float:right;}
 </style>
--- src/components/books/BookEdit_JSON.vue
+++ src/components/books/BookEdit_JSON.vue
@@ -1,101 +1,108 @@
 <template>
+
   <div class='bookeditor'>
-
     <h3>Meta Data</h3>
-<div class="box">
-   <button id="save" @click="savemeta" class=" button save fa fa-floppy-o"></button>
-   <pre id="metadata" contenteditable="true" class="json-text " v-html="metajson"></pre>
-</div>
+      <div class="box">
+         <button id="save" @click="saveMeta" class=" button save fa fa-floppy-o"></button>
+         <pre id="metadata" contenteditable="true" class="json-text " v-html="metaHTML"></pre>
+      </div>
 
-<h3>Content</h3>
-<div>    
-   <button id="save2" @click="savecontent" class=" button save fa fa-floppy-o"></button>    
-    <pre id="contentdata" contenteditable="true" class="json-text" v-html="contentjson"></pre>
-</div>
-
+      <h3>Content</h3>
+      <div>    
+         <button id="save2" @click="saveContent" class=" button save fa fa-floppy-o"></button>    
+          <pre id="contentdata" contenteditable="true" class="json-text" v-html="contentHTML"></pre>
+      </div>
   </div>
+
 </template>
 
 <script>
- 
+  import Vue from 'vue'
+  import access from "../../mixins/access.js"
+  import PouchDB from 'pouchdb'
 
 export default {
 
   name: 'BookEditJson',
 
   data () {
-    return {
-      data: '',
-      tagtypes: {
-        par: 'p',
-        header: 'h1',
-        subheader: 'h3'
-      },
-      componentState: 'start'
-    }
+    return { 
+      componentState: 'start', 
+      metaHTML: "<b>No Meta!</>",
+      contentHTML: "<b>No Content!</>"
+    }  
   },
 
-  components: {
-  },
-
+  mixins: [access],
+  
   methods: {
-    blockEnter: function(msg, e) {
-      if(e) {
-        e.preventDefault();
-        e.stopPropagation();
-      }  
+    // converts JSON object into pretty formated string
+    formatedJSON: function(x) {
+      let str = JSON.stringify(x, undefined, 4);
+      return str.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+      function (match) {
+        let cls = 'num';
+        if (/^"/.test(match)) { if (/:$/.test(match)) { cls = 'key';} else { cls = 'string';} }  
+        else if (/true|false/.test(match)) { cls = 'boolean';}
+        else if (/null/.test(match)) {cls = 'null';}
+        return '<span class="' + cls + '">' + match + '</span>';
+      });
     },
 
-    savemeta: function() { 
-      let t =  document.getElementById('metadata').textContent;
-      try { this.$store.state.currentBookMeta = JSON.parse(t); }
-      catch(e) { alert('An error has occurred: '+e.message);}
+    saveMeta: function() { 
+      let vm = this
+      let t =  document.getElementById('metadata').textContent
+      try { 
+        let data = JSON.parse(t)
+        let db = this.libraryDBmeta()
+        db.get(data._id).then(function(doc){ // get new version doc        
+          data._rev = doc._rev // update new data with new _rev 
+          db.put(data).then(data => { // post up the new data and return the newer _rev
+            db.get(data.id).then(newdoc => {                                 
+              vm.$store.state.currentBookMeta = JSON.parse(JSON.stringify(newdoc))             
+            })
+          }).catch(err => console.log(err))
+        }) 
+      }
+      catch(e) {
+        alert('An error has occurred: '+e.message)
+      }
     },
 
-    savecontent: function() {
-      let t = document.getElementById('contentdata').textContent;
-      try { this.$store.state.currentBook = JSON.parse(t); }
-      catch(e) { alert('An error has occurred: '+e.message);}
+    saveContent: function() {
+      let vm = this
+      let t =  document.getElementById('contentdata').textContent
+      try { 
+        let data = JSON.parse(t)
+        let db = this.libraryDB()
+        db.get(data._id).then(function(doc){
+          data._rev = doc._rev
+          db.put(data).then(data => {
+            db.get(data.id).then(newdoc => { 
+              vm.$store.state.currentBook = JSON.parse(JSON.stringify(newdoc))
+            })  
+          }).catch(err => console.log(err))  
+        })
+      }    
+      catch(e) { 
+        alert('An error has occurred: '+e.message);
+      } 
     }
-
   },
 
   computed: {
-    currentBook: function() {
-      return this.$store.getters.currentBook
-    },
-
-    currentBookContentBlocks: function () {
-      if (this.$store.getters.currentBook && this.$store.getters.currentBook.hasOwnProperty('content_blocks')) return this.$store.getters.currentBook.content_blocks
-      else return []
-    },
-
-    metajson: function() {
-      let x = Object.assign({}, this.$store.state.currentBookMeta);
-      let str = JSON.stringify(x, undefined, 4);
-      return str.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
-        function (match) {
-          let cls = 'num';
-          if (/^"/.test(match)) { if (/:$/.test(match)) { cls = 'key';} else { cls = 'string';} }  
-          else if (/true|false/.test(match)) { cls = 'boolean';}
-          else if (/null/.test(match)) {cls = 'null';}
-          return '<span class="' + cls + '">' + match + '</span>';
-        });
-
-    },
-
-    contentjson: function() {
-      let x = Object.assign({}, this.$store.state.currentBook);
-      let str = JSON.stringify(x, undefined, 4);
-      return str.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
-        function (match) {
-          let cls = 'num';
-          if (/^"/.test(match)) { if (/:$/.test(match)) { cls = 'key';} else { cls = 'string';} }  
-          else if (/true|false/.test(match)) { cls = 'boolean';}
-          else if (/null/.test(match)) {cls = 'null';}
-          return '<span class="' + cls + '">' + match + '</span>';
-        });
-    }
+    // contentjson: function() {
+    //   let x = Object.assign({}, this.$store.state.currentBook);
+    //   let str = JSON.stringify(x, undefined, 4);
+    //   return str.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    //     function (match) {
+    //       let cls = 'num';
+    //       if (/^"/.test(match)) { if (/:$/.test(match)) { cls = 'key';} else { cls = 'string';} }  
+    //       else if (/true|false/.test(match)) { cls = 'boolean';}
+    //       else if (/null/.test(match)) {cls = 'null';}
+    //       return '<span class="' + cls + '">' + match + '</span>';
+    //     });
+    // }
   },
 
 
@@ -110,6 +117,8 @@ export default {
   },
   mounted: function() {
     this.componentState = 'mounted'
+    this.metaHTML = this.formatedJSON(this.$store.state.currentBookMeta)
+    this.contentHTML = this.formatedJSON(this.$store.state.currentBook)
   },
 }
 </script>
