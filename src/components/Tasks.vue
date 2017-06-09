10d1f5481597b1949e97969dffe1fd1ee579d3b9
ylis <ylis@isddesign.com>
2017-06-09 10:09:30 +0200

    Tasks list, add task modal

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,117 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-  </div>
+  <vue-tabs>
+    <v-tab title="My Tasks">
+      <div class="table toolbar">
+        <div class="tr">
+          <div class='td'>
+            <h3><i class="fa fa-arrow-circle-o-right"></i>&nbsp;{{tasks.total}} Current tasks in your Queue</h3>
+          </div>
+          <div class='td'>
+            <button @click='taskAddModalActive = true' class='btn btn-default'>
+              <i class="fa fa-plus"></i>&nbsp;New Task
+            </button>
+          </div>
+        </div>
+      </div>
+      <task-add-modal 
+        :show="taskAddModalActive"
+        :task_types="task_types"
+        :users="users"
+        @closed="taskAddModalClose">
+        
+      </task-add-modal>
+      <div v-for="task in tasks.list" class="tasks-box table">
+        <div class="task-type tr">
+          <div class="td">
+            <h2><i class="fa fa-book"></i>&nbsp;{{task.type}}&nbsp;({{task.list.length}})</h2>
+          </div>
+        </div>
+        <div v-for="subtask in task.list" class="subtasks-box tr">
+          <div class="subtask-title-box td">
+            <h4>({{subtask.list.length}})&nbsp;{{subtask.type}}</h4>
+          </div>
+          <div class="subtask-items-box td">
+            <div v-for="item in subtask.list" class="subtask-item-box">
+              <a :href="'/books/edit/' + item.book_id">{{item.book_title}}&nbsp;<i class="fa fa-arrow-circle-o-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </v-tab>
+    <v-tab title="Work History"></v-tab>
+  </vue-tabs>
 </template>
 
 <script>
+import { VueTabs, VTab } from 'vue-nav-tabs'
+import 'vue-nav-tabs/dist/vue-tabs.min.css'
+import axios from 'axios'
+import TaskAddModal from './tasks/TaskAddModal'
+const TASKS_URL = '/static/tasks.json'//process.env.ILM_API + '/api/v1/tasks'
+const TASK_TYPES_URL = '/static/task_types.json'//process.env.ILM_API + '/api/v1/tasks/types'
+const TASK_USERS_URL = process.env.ILM_API + '/api/v1/tasks/users'
 export default {
   data () {
     return {
-      msg: 'Assignments'
+      msg: 'Assignments',
+      taskAddModalActive: false,
+      tasks: {
+        list: [],
+        total: 0
+      },
+      task_types: [],
+      users: {
+        'editor': [],
+        'proofer': [],
+        'engineer': [],
+        'reader': []
+      }
+    }
+  },
+  
+  components: {
+    VueTabs,
+    VTab,
+    TaskAddModal
+  },
+  
+  mounted() {
+    this.getTasks()
+    this.getTaskTypes()
+    this.getTaskUsers()
+  },
+  
+  methods: {
+    getTasks() {
+      var self = this
+      axios.get(TASKS_URL).then(tasks => {
+        self.tasks = tasks.data
+      })
+      .catch(error => {})
+    },
+    taskAddModalClose(create) {
+      this.taskAddModalActive = false
+    },
+    getTaskTypes() {
+      var self = this
+      axios.get(TASK_TYPES_URL).then(types => {
+        self.task_types = types.data
+      })
+      .catch(error => {})
+    },
+    getTaskUsers() {
+      var self = this
+      axios.get(TASK_USERS_URL).then(users => {
+        for (var role in self.users) {
+          self.users[role] = []
+          for (var i in users.data) {
+            if (users.data[i].roles.indexOf(role) != -1) {
+              self.users[role].push(users.data[i])
+            }
+          }
+        }
+      })
+      .catch(error => {})
     }
   }
 }
@@ -16,4 +119,73 @@ export default {
 
 
 <style scoped>
+.toolbar {
+   width: 100%;
+   position: relative;
+   padding-left: .25em;
+   padding-right: .25em;
+   box-shadow: 0px 0px 3px 2px rgba(178, 191, 224, 0.53);
+   padding: 5px;
+ }
+.toolbar td {
+   text-align: left;
+   padding-top:0; margin-top:0;
+ }
+.toolbar td.right {
+  text-align: right;
+  position: inline;
+  padding-top: 11px;
+  float: right;
+  padding-right: 10px;
+}
+.toolbar h3 {
+  margin: 0px;
+}
+.toolbar h3 i.fa {
+  font-size: 30px;
+}
+.table {
+  display: table;
+  /*border-spacing: 15px;*/
+  padding: 0;
+}
+.tr {
+ display: table-row;
+}
+.td {
+  display: table-cell;
+  vertical-align: middle;
+  white-space: nowrap;
+  padding: 5px;
+}
+.tasks-box {
+  width: 100%;
+  margin-left: 35px;
+}
+.tasks-box .task-type {
+  
+}
+.subtasks-box {
+  display: table-row;
+}
+.subtask-title-box {
+  padding: 5px 25px 5px 5px;
+  width: 5em;
+}
+.subtask-items-box {
+  
+}
+.subtask-item-box {
+  padding: 5px 10px;
+}
+.tasks-box .task-type i.fa {
+  font-size: 35px;
+  margin-left: -35px;
+}
+.subtask-list-box {
+  display: inline-block;
+}
+.subtasks-box:nth-of-type(even) {
+  background-color: #f9f9f9;
+}
 </style>
---
+++ src/components/tasks/TaskAddModal.vue
@@ -0,0 +1,204 @@
+<template>
+  <modal id="taskAddModal" :value="show" effect="fade" @closed="closed">
+    <div slot="modal-header" class="modal-header">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="cancel"><span aria-hidden="true">×</span></button>
+      <h4 class="modal-title">Add Task</h4>
+    </div>
+    <div slot="modal-body" class="modal-body">
+      <div class="form-group">
+        <label>Type</label>
+        <select v-model="type" class="form-control">
+          <option v-for="task_type in task_types" :value="task_type.id">{{task_type.title}}</option>
+        </select>
+        <div v-if="errors.type" v-for="err in errors.type" class="error-message" v-text="err"></div>
+      </div>
+      <div class="form-group" v-if="false">
+        <label>Subtype</label>
+        <select class="form-control" v-model="subtype">
+          <option v-for="task_subtype in subtypes" :value="task_subtype.id">{{task_subtype.title}}</option>
+        </select>
+        <div v-if="errors.subtype" v-for="err in errors.subtype" class="error-message" v-text="err"></div>
+      </div>
+      <div class="form-group" v-if="showField('name')">
+        <label>Book</label>
+        <input type="text" class="form-control" v-model="name" />
+        <div v-if="errors.name" v-for="err in errors.name" class="error-message" v-text="err"></div>
+      </div>
+      <div class="form-group" v-if="showField('roles.editor')">
+        <label>Editor</label>
+        <select class="form-control" v-model="roles['editor']">
+          <option v-for="user_editor in users['editor']" :value="user_editor._id">{{user_editor.email}}</option>
+        </select>
+        <div v-if="errors['roles.editor']" v-for="err in errors['roles.editor']" class="error-message" v-text="err"></div>
+      </div>
+      <div class="form-group" v-if="showField('roles.reader')">
+        <label>Reader</label>
+        <select class="form-control" v-model="roles['reader']">
+          <option v-for="user_reader in users['reader']" :value="user_reader._id">{{user_reader.email}}</option>
+        </select>
+        <div v-if="errors['roles.reader']" v-for="err in errors['roles.reader']" class="error-message" v-text="err"></div>
+      </div>
+      <div class="form-group" v-if="showField('roles.proofer')">
+        <label>Proofer</label>
+        <select class="form-control" v-model="roles['proofer']">
+          <option v-for="user_proofer in users['proofer']" :value="user_proofer._id">{{user_proofer.email}}</option>
+        </select>
+        <div v-if="errors['roles.proofer']" v-for="err in errors['roles.proofer']" class="error-message" v-text="err"></div>
+      </div>
+      <div class="form-group" v-if="showField('roles.engineer')">
+        <label>Engineer</label>
+        <select class="form-control" v-model="roles['engineer']">
+          <option v-for="user_engineer in users['engineer']" :value="user_engineer._id">{{user_engineer.email}}</option>
+        </select>
+        <div v-if="errors['roles.engineer']" v-for="err in errors['roles.engineer']" class="error-message" v-text="err"></div>
+      </div>
+    </div>
+    <div slot="modal-footer" class="modal-footer">
+      <button class="btn btn-primary" type="button" @click="save">Submit </button>
+    </div>
+  </modal>
+</template>
+<script>
+import { modal } from 'vue-strap'
+import modalMixin from './../../mixins/modal'
+import axios from 'axios'
+const TASKS_URL = process.env.ILM_API + '/api/v1/tasks'
+export default {
+  name: 'TaskAddModal',
+  components: {
+    modal
+  },
+  mixins: [modalMixin],
+  props: {
+    show: Boolean,
+    task_types: Array,
+    users: Object
+  },
+  data() {
+    return {
+      type: '',
+      subtype: '',
+      subtypes: [],
+      roles: {},
+      name: '',
+      fields_by_type: {
+        '1': {
+          'name': {'require': true},
+          'roles': {
+            'editor': {'require': true}, 
+            'proofer': {'require': true}, 
+            'engineer': {'require': true}
+          }
+        },
+        '2': {
+          'name': {'require': true},
+          'roles': {
+            'reader': {'require': true}, 
+            'proofer': {'require': true}, 
+            'engineer': {'require': true}
+          }
+        }
+      },
+      errors: {}
+    }
+  },
+  methods: {
+    cancel() {
+      var self = this
+      self.$emit('closed', false)
+    },
+    save() {
+      if (!this.validate()) {
+        return false
+      }
+      var self = this
+      //console.log(self.roles)
+      var task = {
+        name: self.name,
+        type: self.type,
+        //subtype: self.subtype,
+        roles: self.roles
+      }
+      axios.post(TASKS_URL, task)
+        .then(response => {
+          self.$emit('closed', true)
+        })
+        .catch(error => {
+        })
+    },
+    showField(name) {
+      if (this.type && this.fields_by_type[this.type]) {
+        let names = name.split('.')
+        let check = this.fields_by_type[this.type]
+        for (let i in names) {
+          if (!check[names[i]]) {
+            return false
+          }
+          check = check[names[i]]
+        }
+        return true
+      } else {
+        return false
+      }
+    },
+    validate() {
+      this.errors = {}
+      console.log(this.roles)
+      if (!this.type) {
+        if (!this.errors['type']) {
+          this.errors['type'] = []
+        }
+        this.errors['type'].push('Required')
+      } else {
+        for (var field in this.fields_by_type[this.type]) {
+          let check = this.fields_by_type[this.type][field]
+          if (Object.keys(check).indexOf('require') !== -1) {
+            if (check['require'] === true && !this[field]) {
+              if (!this.errors[field]) {
+                this.errors[field] = []
+              }
+              this.errors[field].push('Required')
+            }
+          } else {
+            for (let i in check) {
+              if (check[i]['require'] === true && !this[field][i]) {
+                if (!this.errors[field + '.' + i]) {
+                  this.errors[field + '.' + i] = []
+                }
+                this.errors[field + '.' + i].push('Required')
+              }
+            }
+          }
+        } 
+      }
+      return Object.keys(this.errors).length == 0
+    }
+  },
+  computed: {
+    
+  },
+  watch: {
+    type(val) {
+      var self = this
+      var selected = this.task_types.filter(t => {
+        return t.id == val
+      })
+      if (selected && selected.length == 1) {
+        self.subtypes = selected[0].subtypes
+      }
+    },
+    show() {
+      this.type = ''
+      this.subtype = ''
+      this.roles = {}
+      this.name = ''
+      this.errors = {}
+    }
+  }
+}
+</script>
+<style scoped>
+.error-message {
+  margin-left: 0%;
+}
+</style>
