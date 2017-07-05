4059d57766f9d20fe2d00d65ce6ed562fa83abcd
Aleksandr Kulykov (olku) <olku@isddesign.com>
2017-07-05 15:01:47 +0300

    Collapsed jobs

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -25,24 +25,28 @@
       <section v-for="job in tasks.list">
         <div class="tr">
           <div class="td task-type">
-            <h2><i class="fa fa-book"></i>&nbsp;{{job.title}}&nbsp;({{job.total}})</h2>
+            <h2><i class="fa fa-book" data-toggle="tooltip" v-bind:title="job.description"></i>&nbsp;{{job.title}}&nbsp;({{job.total}})&nbsp;<i :class="[job.tasksVisible ? 'fa-chevron-up' : 'fa-chevron-down' , 'fa collapsebtn']" aria-hidden="true" @click='job.tasksVisible = !job.tasksVisible'></i></h2>
           </div>
           <!--<div class="td"></div>-->
         </div>
-        <div v-for="task in job.tasks" class="tr subtasks-box">
-          <div class="task-title-box td">
-            <h4>(1)&nbsp;{{task.title}}</h4>
-          </div>
+        <transition name="fade">
+        <section v-if="job.tasksVisible">
+          <div v-for="task in job.tasks" class="tr subtasks-box">
+            <div class="task-title-box td">
+              <h4>({{task.count}})&nbsp;{{task.title}}</h4>
+            </div>
 
-          <div class="subtask-items-box td">
-            <div class="subtask-item-box">
-              <button class="btn btn-default" v-if="!job.bookid && task.type == 1" v-on:click="importBook(job._id)">
-                <i class="fa fa-pencil"></i>Import book "{{job.title}}"
-              </button>
-              <a v-else :href="'/books/edit/' + job.bookid">{{job.bookid}}&nbsp;<i class="fa fa-arrow-circle-o-right"></i></a>
+            <div class="subtask-items-box td">
+              <div class="subtask-item-box">
+                <button class="btn btn-default" v-if="!job.bookid && task.type == 1" v-on:click="importBook(job._id)">
+                  <i class="fa fa-pencil"></i>Import book "{{job.title}}"
+                </button>
+                <a v-else :href="'/books/edit/' + job.bookid">{{job.bookid}}&nbsp;<i class="fa fa-arrow-circle-o-right"></i></a>
+              </div>
             </div>
           </div>
-        </div>
+        </section>
+        </transition>
       </section>
       <!--<section v-for="job in tasks.list">-->
       </div>
@@ -118,7 +122,7 @@ export default {
         let tasks_formatted = {total: 0, list: []};
         let jobs = tasks.data.rows;
         for (let jobId in jobs) {
-          console.log('jobs[jobId].title', jobs[jobId].title);
+
           this.getBookMeta(jobs[jobId].bookid).then(meta => {
               jobs[jobId].title = meta.title;
           }).catch(error => {});
@@ -126,15 +130,22 @@ export default {
           tasks_formatted.list.push(jobs[jobId]);
           jobs[jobId].total = jobs[jobId].tasks.length;
           tasks_formatted.total += jobs[jobId].total;
+          jobs[jobId].tasksVisible = false;
 
-          jobs[jobId].tasks.forEach((elTask)=>{
-              elTask.title = this.task_types.tasks.find((s_type) => {
-                  return s_type._id == elTask.type
-              }).title;
-          });
+          jobs[jobId].tasks = jobs[jobId].tasks.reduce((acc, val)=>{
+            let key = 'type_'+val.type;
+            if (acc.hasOwnProperty(key)) acc[key].count ++;
+            else {
+                val.title = this.task_types.tasks.find((s_type) => {
+                    return s_type._id == val.type
+                }).title;
+                acc[key] = {count:1, ...val};
+            }
+            return  acc;
+          }, {} );
 
         }
-        this.tasks = tasks_formatted
+        this.tasks = tasks_formatted;
       })
       .catch(error => {})
     },
@@ -249,11 +260,27 @@ export default {
 .subtask-item-box {
   padding: 5px 10px;
 }
-.tasks-box .task-type i.fa {
+.tasks-box .task-type i.fa.fa-book{
   font-size: 35px;
   margin-left: -35px;
 }
 .subtasks-box:nth-of-type(even) {
   background-color: #f9f9f9;
+}
+
+.collapsebtn {
+    width: 30px; height: 30px;
+    display: inline-block;
+    background: white;
+    padding: 5px;
+    border: .5px solid rgb(204, 212, 226);
+    border-radius: 25px;
+    box-shadow: 0px 0px 5px 1px rgba(0,0,0,0.2);
+    color: rgba(204, 212, 226, 1);
+    cursor: pointer;
+
+    &:hover {
+      color: green;
+    }
 }
 </style>
