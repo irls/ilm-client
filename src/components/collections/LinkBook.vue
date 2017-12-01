32d709a6d5ddfa4533d123ccf318dc8d0f8dbf83
ylis <ylis@isddesign.com>
2017-12-01 14:58:05 +0200

    Scrolling books list

--- src/components/collections/LinkBook.vue
+++ src/components/collections/LinkBook.vue
@@ -287,4 +287,22 @@
 .modal-footer {
 
 }
+#books_grid {
+  height: 60%;
+  overflow: scroll;
+}
+#books_grid::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+  border-radius: 10px;
+  background-color: #F5F5F5;
+}
+#books_grid::-webkit-scrollbar {
+  width: 12px;
+  background-color: #F5F5F5;
+}
+#books_grid::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+  background-color: #555;
+}
 </style>
