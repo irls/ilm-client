2ff51d4deabb6f19f96990a4f51a12d62844cd07
ylis <ylis@isddesign.com>
2018-04-30 14:35:16 +0300

    Recount on voicework change

--- src/components/books/BookBlockView.vue
+++ src/components/books/BookBlockView.vue
@@ -886,7 +886,8 @@ export default {
         'tc_approveBookTask',
         'setCurrentBookBlocksLeft',
         'setCurrentBookCounters',
-        'addBlockLock'
+        'addBlockLock',
+        'getAlignCount'
       ]),
       //-- Checkers -- { --//
       isCanFlag: function (flagType = false, range_required = true) {
@@ -2454,6 +2455,7 @@ export default {
             this.voiceworkUpdating = false;
             if (response.status == 200) {
               this.$root.$emit('from-bookblockview:voicework-type-changed');
+              this.getAlignCount();
               response.data.updField = 'voicework';
               this.$root.$emit('bookBlocksUpdates', response.data);
               //this.setCurrentBookBlocksLeft(this.block.bookid);
