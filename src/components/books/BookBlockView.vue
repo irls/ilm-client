b36cc0af1f8982ecf8e942ffcd8f2fe51a9e278f
ylis <ylis@isddesign.com>
2018-09-28 13:19:11 +0300

    ILM-942

--- src/components/books/BookBlockView.vue
+++ src/components/books/BookBlockView.vue
@@ -2394,7 +2394,6 @@ export default {
         this.$root.$off('from-audioeditor:closed', this.evFromAudioeditorClosed);
 
         Vue.nextTick(() => {
-          console.log('open');
           let audiosrc = footnoteIdx !== null ? this.block.getAudiosrcFootnote(footnoteIdx, 'm4a', true) : this.blockAudio.src;
           let text = footnote ? footnote.content : this.blockAudio.map;
           let loadBlock = footnoteIdx !== null ? {_id: this.check_id, voicework: footnote ? footnote.voicework : 'tts'} : this.block;
@@ -3642,7 +3641,7 @@ export default {
         /*cursor: pointer*/
       }
 
-      w:not([data-map]) {
+      w:not([data-map]), w.alignment-changed {
         background: linear-gradient(
             transparent,
             transparent 30%,
