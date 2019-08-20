8a39156629d0cc71a57b3ba7b6992088259253ed
vzhu <vzhu@isddesign.com>
2019-08-20 14:09:46 +0300

    re-fix ILM-2257 after lost in merge process

--- src/components/books/BookAudioIntegration.vue
+++ src/components/books/BookAudioIntegration.vue
@@ -18,7 +18,7 @@
                   <span v-on:click="listSort('date', 'desc')">Date imported</span>
                 </li>
             </dropdown>
-            <dropdown text="" type="default" ref="allAudioDropdownFilter" class="all-audio-dropdown aad-filter">
+            <dropdown text="" :type="this.aad_filter == 'all' ? 'default' : 'button'" ref="allAudioDropdownFilter" class="all-audio-dropdown aad-filter">
                 <li :class="this.aad_filter != 'pending' && this.aad_filter != 'aligned' && this.aad_filter != 'filename' ? ' aad_selected' : ' '">
                   <span v-on:click="filterAll()">All</span>
                 </li>
