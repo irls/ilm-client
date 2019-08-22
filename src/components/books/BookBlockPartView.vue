35527e8b4fa1dd6c5099c11ed34b8b8eb8877041
ylis <ylis@isddesign.com>
2019-08-22 08:55:53 +0300

    <bug>(ILM-2113): hide numbered list

--- src/components/books/BookBlockPartView.vue
+++ src/components/books/BookBlockPartView.vue
@@ -1051,8 +1051,8 @@ export default {
             toolbar = {
                 buttons: [
                   'bold', 'italic', 'underline',
-                  //'superscript', 'subscript',
-                  'orderedlist', 'unorderedlist',
+                  //'superscript', 'subscript','orderedlist', 
+                  'unorderedlist',
                   //'html', 'anchor',
                   'quoteButton', 'suggestButton'
                 ]
