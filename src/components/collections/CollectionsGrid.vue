728488a63f5bb7f6659251d287ad36135822c798
vzhu <vzhu@isddesign.com>
2019-11-18 11:46:26 +0200

    ILM-2327 dummy book

--- src/components/collections/CollectionsGrid.vue
+++ src/components/collections/CollectionsGrid.vue
@@ -139,10 +139,14 @@
                   return _b.bookid === b;
                 });
                 if (book) {
-                  if (book.importStatus == 'staging' && book.blocksCount < 2){
-                    book.importStatus = 'staging_empty'
+                  if (book.importStatus == 'staging' && book.blocksCount <= 2){
+                    if (!book.hasOwnProperty('publishLog') || book.publishLog == null){
+                      book.importStatus = 'staging_empty'
+                    } else if (!book.publishLog.updateTime){
+                      book.importStatus = 'staging_empty'
+                    }
                   }
-                  books.push(book);
+                  result.push(book)
                 }
               });
               c.books_list = books;
