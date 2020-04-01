7a17cf0124096491428456ba01efa0a40a906167
ylis <ylis@isddesign.com>
2020-04-01 20:37:19 +0300

    <bug>(ILM-2992)

--- src/components/books/BookBlockPartView.vue
+++ src/components/books/BookBlockPartView.vue
@@ -79,11 +79,11 @@
                 </div>
               </div>
               <div class="table-cell -content-wrapper"  :forced_bind="blockPart.blockId">
-                <hr v-if="block.type=='hr'"
+                <hr v-if="blockType=='hr'"
                   :class="[block.getClass(mode), {'checked': blockO.checked}]"
                   @click="onClick($event)"/>
 
-                <div v-else-if="block.type == 'illustration'"
+                <div v-else-if="blockType == 'illustration'"
                 :class="['table-body illustration-block', {'checked': blockO.checked}]"
                 @click="onClick($event)" :key="blockIllustration">
                   <img v-if="hasIllustration" :src="blockIllustration"
@@ -767,7 +767,8 @@ export default {
           adminOrLibrarian: 'adminOrLibrarian',
           currentJobInfo: 'currentJobInfo',
           mode: 'bookMode',
-          blockLockType: 'blockLockType'
+          blockLockType: 'blockLockType',
+          storeListById: 'storeListById'
       }),
       getBlockLang: {
         cache: false,
@@ -806,6 +807,12 @@ export default {
       blockIllustration: {
         get() {
           return this.block.getIllustration();
+        },
+        cache: false
+      },
+      blockType: {
+        get() {
+          return this.block.blockid ? this.storeListById(this.block.blockid).type : '';
         },
         cache: false
       }
