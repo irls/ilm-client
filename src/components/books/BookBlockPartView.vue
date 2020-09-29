5111dbd71781d257afc0b6f26c147b170f756001
ylis <ylis@isddesign.com>
2020-09-29 10:36:14 +0300

    <bug>(ILM-3556)

--- src/components/books/BookBlockPartView.vue
+++ src/components/books/BookBlockPartView.vue
@@ -1767,6 +1767,8 @@ Save audio changes and realign the Block?`,
           content = content.replace(/<br\/?><p[^>]*><\/p>/gm, '<br>');
           content = content.replace(/<p[^>]*>([\s\S]*?)<br[^>]*><\/p>/gm, '<p>$1</p>').replace(/<div[^>]*>([\s\S]*?)<br\/?><\/div>/img, '<div>$1<\/div>');
           content = content.replace(/<\/p><p[^>]*>/img, '<br>')/*.replace(/(<div[^>]*>)<p[^>]*><br\/?><\/p>/img, '$1')*/;
+          content = content.replace(/<div[^>]*><w[^>]*><\/w><\/div>/mg, ''); // for aligned blocks editor creates empty <w></w> tags on line break adding
+          content = content.replace(/<w[^>]*><\/w>/mg, '');
           content = content.replace(/(<br\/?>)<div[^>]*><p[^>]*>([\s\S]*?)<\/p>([\s\S]*?)<\/div>/img, '$1$2<br>$3');
           content = content.replace(/<div[^>]*><p[^>]*>([\s\S]*?)<\/p><\/div>$/img, '<br>$1');
           content = content.replace(/<div[^>]*><p[^>]*>([\s\S]*?)<\/p>([\s\S]*?)<\/div>/img, '<br>$1<br>$2');
