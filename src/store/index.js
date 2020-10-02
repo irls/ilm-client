4fabf46b937b3a3bc7a0db885872e75841e13052
ylis <ylis@isddesign.com>
2020-10-02 16:07:12 +0300

    <bug>(ILM-3563)

--- src/store/index.js
+++ src/store/index.js
@@ -3546,6 +3546,7 @@ export const store = new Vuex.Store({
                 if (block) {
                   block.setUpdated(b.updated);
                   block.setPauseBefore(b.pause_before);
+                  block.status.marked = b.status.marked;
                 }
               });
             }
