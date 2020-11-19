7a9bf198fa6d881521d55e317eb6a0fb0f709b32
ylis <ylis@isddesign.com>
2020-11-19 09:56:56 +0200

    <bug>(ILM-3579)

--- src/components/books/BookBlockView.vue
+++ src/components/books/BookBlockView.vue
@@ -2249,6 +2249,9 @@ Save audio changes and realign the Block?`,
             if (this.$refs && this.$refs.blocks[blockPartIdx]) {
               this.$refs.blocks[blockPartIdx].isSaving = false;
             }
+            if (this.block.parts[blockPartIdx]) {
+              this.block.parts[blockPartIdx].isSaving = false;
+            }
             return Promise.resolve(response);
           })
           .catch(err => {
@@ -2935,7 +2938,7 @@ Save text changes and realign the Block?`,
           }
         });
         let pos = this.updFootnotes(this.block.footnotes.length + 1);
-        this.block.footnotes.splice(pos, 0, new FootNote({}));
+        this.block.addFootnote(pos);
         this.$forceUpdate();
         this.isChanged = true;
         let ref = this.$refs['footnoteContent_' + pos];
@@ -2947,6 +2950,7 @@ Save text changes and realign the Block?`,
           //this.destroyEditor();
           this.initFtnEditor(true);
         });
+        this.$store.commit('set_storeList', this.block);
       },
       delFootnote: function(pos, checkText = true) {
         if (checkText) {
@@ -4745,7 +4749,7 @@ Save text changes and realign the Block?`,
             });
             this.recountVoicedBlocks();
           }
-          this.block.isChanged = val;
+          this.block.setChanged(val);
           this.recountApprovedInRange();
         }
       },
