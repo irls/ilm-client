59ff9cf288f43c705b7274e72e7ddf4a4a867452
vagrant <vagrant@ilm-workstation.local>
2020-11-19 13:02:21 +0200

    <task>(ILM-3371):Fronted update

--- src/store/index.js
+++ src/store/index.js
@@ -196,9 +196,16 @@ export const store = new Vuex.Store({
     bookDifficulties: [
       '1',
       '2',
+      '3',
       '4',
+      '5',
       '6',
-      '11'
+      '7',
+      '8',
+      '9',
+      '10',
+      '11',
+      '12'
     ],
     loadBookWait: null,
     loadBookTaskWait: null,
