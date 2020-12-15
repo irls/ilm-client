3c71493cc9461e57308b7b35b56b50a1db5f9230
vagrant <vagrant@ilm-workstation.local>
2020-12-15 15:43:29 +0200

    <task>(ILM-3690):fix

--- src/components/books/BookMetaEdit.vue
+++ src/components/books/BookMetaEdit.vue
@@ -789,6 +789,8 @@ export default {
       handler (val) {
         this.init();
         this.lockLanguage = false;
+        this.currentBookMeta.difficulty = this.currentBookMeta.difficulty? parseFloat(currentBookMeta.difficulty).toFixed(2): this.currentBookMeta.difficulty;
+
       },
       deep: true
     },
