1026b7458f5806e8cd5c15ee28b0ed0c3d39402e
ylis <ylis@isddesign.com>
2021-03-23 13:33:43 +0200

    <bug>(ILM-3710): corrected cursor position

--- src/components/generic/medium-editor/js/medium-editor.js
+++ src/components/generic/medium-editor/js/medium-editor.js
@@ -1578,6 +1578,20 @@ MediumEditor.extensions = {};
 
         replaceStringAt: function (input, position, replace) {
             return input.substring(0, position) + replace + (position >= input.length ? '' : input.substring(position + 1));
+        },
+
+        insertTextAtCursor: function (text) {
+            let selection = window.getSelection(),
+            range = selection.getRangeAt(0),
+            node = document.createTextNode(text);
+            range.deleteContents();
+            range.insertNode(node);
+
+            for (let position = 0; position !== text.length; position++) {
+                selection.modify('move', 'right', 'character');
+            }
+            node.parentNode.normalize();// merge neighbour text elements
+            //console.log(rootNode);
         }
     };
 
@@ -6981,6 +6995,16 @@ MediumEditor.extensions = {};
                 }
             }
             return false;
+        } else {
+            let element = document.getSelection().anchorNode,
+            rootNode = element.parentNode;
+            while (rootNode.nodeName !== 'DIV') {
+                rootNode = rootNode.parentNode;
+            }
+            let isList = MediumEditor.util.isElementWhitespaceStyle(rootNode, ['pre-line']);
+            if (isList) {// when adding content to contenteditable element with style whitespace: pre-line, browser removes line breaks
+                return false;
+            }
         }
         return false;
     }
@@ -7037,7 +7061,7 @@ MediumEditor.extensions = {};
             } else if (element.parentNode.nodeName === 'DIV') {// click inside usual text node
                 if (isList) {
                     element.nodeValue = selection + br + (afterSelection.length > 0 || element.nextSibling || selection.match(new RegExp(`${br}$`)) ? afterSelection : br);
-                    MediumEditor.selection.moveCursor(this.options.ownerDocument, element, afterSelection.length === 0 ? selection.length + 1 : selection.length + 1);
+                    MediumEditor.selection.moveCursor(this.options.ownerDocument, element, afterSelection.length === 0 ? selection.length : selection.length);
                 } else {
                     if (element.nodeName === 'BR') {
                         //console.log('HERE1');
@@ -7134,6 +7158,7 @@ MediumEditor.extensions = {};
                         let partThree = partTwo.cloneNode();
                         baseNode.insertBefore(partThree, partTwo);
                         //console.log('HERE6');
+                        //MediumEditor.selection.moveCursor(this.options.ownerDocument, partTwo, 0);
                     }
                 }
             }
@@ -7146,6 +7171,25 @@ MediumEditor.extensions = {};
             });
 
             rootNode.dispatchEvent(eventInput);// run event for the element events handling
+        } else {
+            let element = document.getSelection().anchorNode,
+            rootNode = element.parentNode;
+            while (rootNode.nodeName !== 'DIV') {
+                rootNode = rootNode.parentNode;
+            }
+            let isList = MediumEditor.util.isElementWhitespaceStyle(rootNode, ['pre-line']);
+            if (isList) {// when adding content to conteneditable element with style whitespace: pre-line browser replaces line breaks
+                //console.log(MediumEditor.util.getKeyCode(event));
+                MediumEditor.util.insertTextAtCursor(String.fromCharCode(MediumEditor.util.getKeyCode(event)));
+                event.preventDefault();
+                var eventInput = new Event('input', {
+                    bubbles: true,
+                    cancelable: true
+                });
+
+                rootNode.dispatchEvent(eventInput);// run event for the element events handling
+                return false;
+            }
         }
     }
 
