1c96d58d503ef970c3142d4f317ddb2abf82a545
olku <olku@solvve.com>
2021-03-23 15:31:12 +0200

    fixup! Add status update

--- src/store/index.js
+++ src/store/index.js
@@ -1946,7 +1946,7 @@ export const store = new Vuex.Store({
               oldBlock.status = response.data.status;
               store.commit('set_storeList', oldBlock);
               state.storeListO.refresh();
-            } else {
+
               state.storeListO.updBlockByRid(response.data.id, {
                 status: response.data.status
               });
