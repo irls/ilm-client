a669063afd70d216d969ff1eb03067356268e706
ylis <ylis@isddesign.com>
2021-08-04 13:05:59 +0300

    <bug>(ILM-4203): wait for promise to finish

--- src/store/index.js
+++ src/store/index.js
@@ -3937,7 +3937,7 @@ export const store = new Vuex.Store({
       return new Promise((resolve, reject) => {
         if (old_out !== new_out) {
           dispatch('getAudioBook');
-          dispatch('getBlocksInRange', [old_out, new_out])
+          return dispatch('getBlocksInRange', [old_out, new_out])
             .then(blocks => {
               if (Array.isArray(blocks)) {
                 blocks.forEach(blk => {
