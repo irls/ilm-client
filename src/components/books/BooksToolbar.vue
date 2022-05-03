338f15d55046409a8dd4636d1cf0fdda239ede51
Vadim Zhuk <vzhu@isddesign.com>
2022-05-03 12:16:29 +0300

    ILM-4544

--- src/components/books/BooksToolbar.vue
+++ src/components/books/BooksToolbar.vue
@@ -13,9 +13,9 @@
   </button>  &nbsp;
 
   <!-- Meta Filter -->
-  <input v-model="filterAuthorTitle" type="text" @keyup="booksFilterChange" class="form-control" style="width: 15em;" placeholder="Filter by Author or Title"></input> <!-- ILM-4544<input @click="filterAuthorTitle=''; $store.commit('SET_CURRENTBOOK_FILTER', {filter: ''})"  type="button" class="btn-inside" value="X"> &nbsp;-->
+  <input v-model="filterAuthorTitle" type="text" @keyup="booksFilterChange" class="form-control" style="width: 15em;" placeholder="Filter by Author or Title"></input> <input @click="filterAuthorTitle=''; $store.commit('SET_CURRENTBOOK_FILTER', {filter: ''})"  type="button" class="btn-inside" value="X">&nbsp;
 
-  <!-- ILM-4544<input v-model="filterTag" type="text" @keyup="booksFilterTagChange" class="form-control" style="width: 18em;" placeholder="Filter by Editor or Project tag"></input><input @click="filterTag=''; $store.commit('SET_CURRENTBOOK_FILTER', {projectTag: ''})"    type="button" class="btn-inside" value="X"> &nbsp;-->
+  <input v-model="filterTag" type="text" @keyup="booksFilterTagChange" class="form-control" style="width: 18em;" placeholder="Filter by Editor or Project tag"></input><input @click="filterTag=''; $store.commit('SET_CURRENTBOOK_FILTER', {projectTag: ''})"    type="button" class="btn-inside" value="X"> &nbsp;
 
   <template v-if="adminOrLibrarian">
     <select @change="booksTypeChange" v-model="bookFilters.jobStatus">
