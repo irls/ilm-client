0ad8410279df8cc0415e0f4f65c771fc0e187069
Semenykhin Maksym <>
2022-11-10 20:53:50 +0200

    debug

--- src/store/bookBlocks.js
+++ src/store/bookBlocks.js
@@ -236,11 +236,17 @@ class BookBlocks {
       this.meta = bookList.meta;
       this.meta.rid = bookList.meta['@rid'];
       if (Array.isArray(bookList.blocks)) {
+        let firstBlock = null;
+
         bookList.blocks.forEach((block)=>{
           if(block.index  === -1){
             console.log('block with negative index');
             console.log(block);
           }else{
+            if(!firstBlock){
+              firstBlock = block;
+            }
+
             this.listIds.push(block.blockid);
             this.listRIds.push(block.rid);
             this.listObjs.push({
@@ -251,7 +257,11 @@ class BookBlocks {
           }
           //this.blocksList[block.blockid] = new BookBlock(block);
         })
-        this.setStartId(bookList.blocks[0].rid)
+        if(!firstBlock){
+          console.error('no block with index found');
+        }
+
+        this.setStartId(firstBlock.rid);
       }
     }
   }
