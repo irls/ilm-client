ada07b8a25c6f40cea9d5f498df1fbf053f3481b
anastasiia-perederii <109763363+anastasiia-perederii@users.noreply.github.com>
2022-12-29 23:09:05 +0200

    test9

--- src/components/collections/CollectionCoverModal.vue
+++ src/components/collections/CollectionCoverModal.vue
@@ -27,9 +27,10 @@
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-globe"></i></span>
                   <input ref="form" type="text" class="form-control" placeholder="URL" v-model="uploadURL" />
-                  
+                  <div>
                   <button class="input-group-addon" data-dismiss="modal" aria-label="Reset" @click="resetInput">
                   <i class="fa fa-trash-o" aria-hidden="true"></i> </button>
+              </div>
               </div>
 
                 <br> &nbsp;&nbsp;&nbsp;  or <br><br>
