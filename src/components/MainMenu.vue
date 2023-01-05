ce5724f352e40e16972c254376372e1f4eb29af5
anastasiia-perederii <109763363+anastasiia-perederii@users.noreply.github.com>
2023-01-05 19:01:07 +0200

    added user name

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -42,7 +42,7 @@
           <!-- <router-link class="presentation" to="/help" tag='li'><a>Help</a></router-link></router-link> -->
           <!-- <router-link class="presentation" to="/contact" tag='li'><a>Contact</a></router-link> -->
 
-          <li class="welcomeName">Welcome, {{$store.state.user.name}}!</li>
+          <li class="welcomeName">Welcome, {{ $store.state.user.name != undefined && $store.state.user.name != "" ? $store.state.user.name : $store.state.name }}!</li>
           <li v-if="$store.state.isLoggedIn" class="presentation"><a href="#" v-on:click.stop.prevent="$store.state.auth.logout()">Logout</a></li>
       </ul>
     </td></tr>
