739067dfee06a4967d25a960e65f16005e6f3a5e
anastasiia-perederii <109763363+anastasiia-perederii@users.noreply.github.com>
2023-01-16 15:48:33 +0200

    bookcover

--- src/components/books/BookEditCoverModal.vue
+++ src/components/books/BookEditCoverModal.vue
@@ -1,10 +1,12 @@
 <template>
- <!-- <transition name="modal"> -->
-  <modal id="bookEditCoverModal" name="bookEditCoverModal" :adaptive="true" height="auto" :clickToClose="false" @opened="opened">
+  <!-- <transition name="modal"> -->
+  <modal id="bookEditCoverModal" name="bookEditCoverModal" :adaptive="true" height="auto" :clickToClose="false"
+    @opened="opened">
 
     <div class="modal-header">
       <div class="header-title">
-        <img src='/static/bookstack.svg' class='book-logo'> <h3 class="header-h">Book Cover</h3>
+        <img src='/static/bookstack.svg' class='book-logo'>
+        <h3 class="header-h">Book Cover</h3>
       </div>
       <button type="button" class="close" data-dismiss="modal" aria-label="Close" v-on:click="hideModal">
         <i class="fa fa-times-circle-o" aria-hidden="true"></i>
@@ -21,103 +23,103 @@
         </ul>
       </div> -->
 
-      
-        <!-- Selection tabs for Upload or Create forms -->
 
-        <div id="upload_pane" :class="['tab-pane fade in image-upload-wrapper', {active: uploadMode}]">
-          <div class="row">
-            <div class="col-md-12">
+      <!-- Selection tabs for Upload or Create forms -->
 
-              <div class="col-sm-4">
-                <img :src="uploadImage" class="preview_upload" v-show="uploadImage.length>0" />
-                <!-- <img :src="uploadImageBlank" class="preview_upload" v-show="uploadImage.length<1" /> -->
-                <div class='coverimg-wrap' v-show="uploadImage.length<1"></div>
-              </div>
+      <div id="upload_pane" :class="['tab-pane fade in image-upload-wrapper', { active: uploadMode }]">
+        <div class="row">
+          <div class="col-md-12">
 
-              <div class="col-sm-8">
-                <div class="group">
+            <div class="col-sm-4">
+              <img :src="uploadImage" class="preview_upload" v-show="uploadImage.length > 0" />
+              <!-- <img :src="uploadImageBlank" class="preview_upload" v-show="uploadImage.length<1" /> -->
+              <div class='coverimg-wrap' v-show="uploadImage.length < 1"></div>
+            </div>
+
+            <div class="col-sm-8">
+              <div class="group">
 
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-globe"></i></span>
-                  <input type="text" class="form-control" placeholder="URL" v-model="uploadImage" v-on:input="onImageChange" />
+                  <input type="text" class="form-control" placeholder="URL" v-model="uploadImage"
+                    v-on:input="onImageChange" />
                 </div>
 
                 <button class="btn btn-default" v-on:click="resetInput"><i class="fa fa-trash-o"></i></button>
 
-               </div>
-
-                 <br> &nbsp;&nbsp;&nbsp;  or <br><br>
-
-
-                <p v-if="errorCoverFileType" ><span class="alert alert-warning">Please select image file for cover</span></p>
-                <label class='btn btn-default' type="file">
-                  <i class="fa fa-folder-open-o" aria-hidden="true"></i> &nbsp; Browse for bookcover file &hellip;
-                  <input name="coverFile" type="file" 
-                    v-show="false" 
-                    accept="image/*"  
-                    @change="onFilesChange($event)"><br>
-                </label>
               </div>
 
+              <br> &nbsp;&nbsp;&nbsp; or <br><br>
+
+
+              <p v-if="errorCoverFileType"><span class="alert alert-warning">Please select image file for cover</span>
+              </p>
+              <label class='btn btn-default' type="file">
+                <i class="fa fa-folder-open-o" aria-hidden="true"></i> &nbsp; Browse for bookcover file &hellip;
+                <input name="coverFile" type="file" v-show="false" accept="image/*" @change="onFilesChange($event)"><br>
+              </label>
             </div>
+
           </div>
         </div>
+      </div>
 
-        <div id="create_pane" class="tab-pane fade in"  :class="{active: !uploadMode}">
-          <div class="row">
+      <div id="create_pane" class="tab-pane fade in" :class="{ active: !uploadMode }">
+        <div class="row">
 
-            <!-- left column, display preview -->
-            <div class="col-md-4">
-              <div id="bookCoverPreviewMain">
-                <div class="bookCoverPreviewWrap">
-                  <div class="book-cover"><img :src="tmp.coverimg"></div>
-                  <div ref="livePreviewTitle" id="livePreviewTitle" class="ql-editor" v-draggable="tmp.title"></div>
-                  <div ref="livePreviewAuthor" id="livePreviewAuthor" class="ql-editor" v-draggable="tmp.author"></div>
-                </div>
+          <!-- left column, display preview -->
+          <div class="col-md-4">
+            <div id="bookCoverPreviewMain">
+              <div class="bookCoverPreviewWrap">
+                <div class="book-cover"><img :src="tmp.coverimg"></div>
+                <div ref="livePreviewTitle" id="livePreviewTitle" class="ql-editor" v-draggable="tmp.title"></div>
+                <div ref="livePreviewAuthor" id="livePreviewAuthor" class="ql-editor" v-draggable="tmp.author"></div>
               </div>
             </div>
+          </div>
 
-            <!-- right column edit controls -->
-            <div class="col-md-8">
-              <div class="book-list">
-                <!-- <div ref="quillContainerTitle" id="quillContainerTitle"></div> -->
-              </div>
-              <div class="book-list bookCoverCarouselWrap">
-                <!--<carousel-3d :display="7" :width="90" :height="115.5">
+          <!-- right column edit controls -->
+          <div class="col-md-8">
+            <div class="book-list">
+              <!-- <div ref="quillContainerTitle" id="quillContainerTitle"></div> -->
+            </div>
+            <div class="book-list bookCoverCarouselWrap">
+              <!--<carousel-3d :display="7" :width="90" :height="115.5">
                   <slide v-for="(bc, index) in bookcovers" :key="index" :index="index">
                     <img :src="bc.src" @click="selectBookCover(index)">
                   </slide>
                 </carousel-3d>-->
-                <span class="addBookCoverBtnWrap">
-                  <i class="fa fa-plus"></i><i class="fa fa-folder-open-o"></i>
-                  <input type="file" @change="addBookCover"/>
-                </span>
-              </div>
-              <div class="book-list">
-                <!-- <div ref="quillContainerAuthor" id="quillContainerAuthor"></div> -->
-              </div>
+              <span class="addBookCoverBtnWrap">
+                <i class="fa fa-plus"></i><i class="fa fa-folder-open-o"></i>
+                <input type="file" @change="addBookCover" />
+              </span>
             </div>
+            <div class="book-list">
+              <!-- <div ref="quillContainerAuthor" id="quillContainerAuthor"></div> -->
+            </div>
+          </div>
 
-          </div> <!-- row? -->
+          <!-- row? -->
         </div> <!-- Create Pane -->
 
       </div> <!-- tabbed content for create -->
 
       <div id='uploadingMsg' v-show='isUploading'>
-         <h2> {{uploadProgress}}   &nbsp; <i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i> </h2>
+        <h2> {{ uploadProgress }} &nbsp; <i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i> </h2>
       </div>
+
+    </div>
 
 
     <div class="modal-footer">
       <button class="btn" v-on:click="cancel()">Cancel</button>
 
-      <button class="btn btn-primary" type="button" 
-        @click="save">Save</button>
+      <button class="btn btn-primary" type="button" @click="save">Save</button>
     </div>
 
   </modal>
 
-<!-- </transition> -->
+  <!-- </transition> -->
 </template>
 
 <script>
@@ -133,7 +135,7 @@ import axios from 'axios'
 import PouchDB from 'pouchdb'
 import superlogin from 'superlogin-client'
 
-import { mapGetters, mapActions }    from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import Vue from 'vue';
 
 const html2canvas = {};
@@ -154,7 +156,7 @@ const html2canvas = {};
 //   theme: 'snow'
 // }
 
-Vue.use(v_modal, {dialog: true, dynamic: true});
+Vue.use(v_modal, { dialog: true, dynamic: true });
 
 export default {
   name: 'BookEditCoverModal',
@@ -162,7 +164,7 @@ export default {
   mixins: [modalMixin, api_config],
 
   components: {
-    
+
   },
 
   // props: {
@@ -171,7 +173,7 @@ export default {
   // },
   props: ['show', 'img'],
 
-  data () {
+  data() {
     return {
       bookcovers: BOOKCOVERS,
       errorCoverFileType: false,
@@ -207,7 +209,7 @@ export default {
   },
 
   watch: {
-    show (val) {
+    show(val) {
       // this.tmp.coverimg = this.img.coverimg
       this.tmp.title.text = this.img.title
       // this.tmp.title.top = this.img.title.top || 0
@@ -224,7 +226,7 @@ export default {
 
       // console.log("Showing cover editor with book: ", this.img)
       if (val) {
-        
+
         this.showModal();
       } else {
         this.hideModal();
@@ -234,11 +236,11 @@ export default {
 
   },
 
-  activated () {
+  activated() {
     //  this.uploadImage = this.img.coverimg
   },
 
-  mounted () {
+  mounted() {
     // const vm = this
 
     // Quill for Title
@@ -260,12 +262,12 @@ export default {
     ...mapActions([
       'updateBookCover'
     ]),
-    addBookCover (e) {
+    addBookCover(e) {
       console.log('addedBookCover', e.target.files[0])
     },
 
     // Make bookcover selectable on vue-carousel-3d
-    selectBookCover (index) {
+    selectBookCover(index) {
       this.tmp.coverimg = this.bookcovers[index].src
       this.tmp.title.top = this.bookcovers[index].title.top
       this.tmp.title.left = this.bookcovers[index].title.left
@@ -276,7 +278,7 @@ export default {
     },
 
     // for this BookEditCoverModal Component -------------------------------------------------------
-    opened () {
+    opened() {
       this.$nextTick(() => {
         window.dispatchEvent(new Event('resize'))
       })
@@ -284,7 +286,7 @@ export default {
       this.uploadImage = this.img.coverimg || '';
     },
 
-    onFilesChange (e) {
+    onFilesChange(e) {
       this.errorCoverFileType = false
       var files = e.target.files || e.dataTransfer.files
       // console.log('*** onFilesChange', files)
@@ -295,22 +297,22 @@ export default {
         this.createImage(files[0])
       } else {
         this.errorCoverFileType = true;
-        setTimeout(()=>{this.errorCoverFileType = false; console.log('end of timeout');}, 5000)
+        setTimeout(() => { this.errorCoverFileType = false; console.log('end of timeout'); }, 5000)
       }
       this.onImageChange();
     },
 
-    createImage (file) {
+    createImage(file) {
       this.uploadFile = file;
       // console.log('*** Creating new image', file)
       var reader = new FileReader()
-      reader.onload = e => { 
+      reader.onload = e => {
         this.uploadImage = e.target.result;
       }
       reader.readAsDataURL(file)
     },
 
-    uploadNewImageData () {
+    uploadNewImageData() {
       if (this.isUploading) {
         return;
       }
@@ -332,7 +334,7 @@ export default {
       this.isUploading = true;
       this.fileChanged = false;
       this.uploadProgress = 'Uploading file';
-      return this.updateBookCover({formData: formData, config: config})
+      return this.updateBookCover({ formData: formData, config: config })
         .then(doc => {
           this.isUploading = false;
           this.uploadProgress = '';
@@ -346,7 +348,7 @@ export default {
         })
     },
 
-    uploadNewImageURL (url) {
+    uploadNewImageURL(url) {
       // console.log('loading url: ', url)
       var vm = this
       return new Promise((resolve, reject) => {
@@ -363,7 +365,7 @@ export default {
       })
     },
 
-    closeWithDelay () {
+    closeWithDelay() {
       this.hideModal();
     },
 
@@ -379,16 +381,16 @@ export default {
       }
     },
 
-    resetInput () {
-          this.uploadImage = "";
-        },
+    resetInput() {
+      this.uploadImage = "";
+    },
 
-    captureBookImage () {
+    captureBookImage() {
       var source = document.getElementById('bookCoverPreviewMain')
       var img = document.getElementById('bookImagePreview')
       var scale = 1
       img.width = source.clientWidth // display at same size
-      this.captureCanvas(source, scale).then(function(newCanvas){
+      this.captureCanvas(source, scale).then(function (newCanvas) {
         // console.log("got here ok")
         img.src = newCanvas.toDataURL() // defaults to png
       })
@@ -396,7 +398,7 @@ export default {
 
     // Capture and scale functionality
     // https://jsfiddle.net/nkhsLbpf/30/
-    captureCanvas (source, scale) {
+    captureCanvas(source, scale) {
       return new Promise((resolve, reject) => {
         // isolate source with temp copy to root (causes flash)
         var temp = document.body.appendChild(source.cloneNode(true))
@@ -426,10 +428,10 @@ export default {
       })
     },
 
-    cancel () {
+    cancel() {
       this.closed()
     },
-    
+
     showModal() {
       this.$modal.show('bookEditCoverModal');
     },
@@ -529,74 +531,93 @@ export default {
 </style>
 
 <style scoped>
-  button.close i.fa {
-    font-size: 18pt; padding-right: 0; margin-right:-20px;
-  }
+button.close i.fa {
+  font-size: 18pt;
+  padding-right: 0;
+  margin-right: -20px;
+}
 
-  .book-logo {
-    width:50px;
-    height:50px;
-  }
-  .header-h {
-    display: inline;
-  }
-  .header-title {
-    padding-left: 0px;
-    display: inline;
-  }
-  div.modal-header {
-    padding:10px; margin:0;
-    padding-right:0;
-    padding-bottom: 0;
-    border-bottom: 1px solid #e5e5e5 !important;
-  }
-  div.modal-dialog {
-    min-width: 700px !important;
-  }
-  .ql-snow .ql-formats {
-    margin-right: 8px !important;
-  }
-  .ql-snow .ql-formats button {
-    padding-right: 0px;
-  }
+.book-logo {
+  width: 50px;
+  height: 50px;
+}
 
-  #livePreviewTitle, #livePreviewAuthor {
-    font-weight: bold; margin-left: .15em;
-    font-family: Garamond, Baskerville, "Baskerville Old Face", "Hoefler Text", "Times New Roman", serif;
-    font-size: 18pt;
-    line-height: 1em;
-    color:	rgba(139,69,19, .5);
-    text-shadow: 0px 1px 0px rgba(256,256,256, .3),
-                  0px -1px 0px rgba(0,0,0,.2),
-                  0px 0px 2px rgba(255,215,0, 0.5);
-  }
-  #livePreviewAuthor {
-    font-size: 16pt;
-  }
+.header-h {
+  display: inline;
+}
 
-  #bookCoverSmall {
-    min-width: 200px; min-height: 300px; border: 1px dashed gray;
-  }
+.header-title {
+  padding-left: 0px;
+  display: inline;
+}
 
-  /*Tabs control*/
+div.modal-header {
+  padding: 10px;
+  margin: 0;
+  padding-right: 0;
+  padding-bottom: 0;
+  border-bottom: 1px solid #e5e5e5 !important;
+}
 
-  .row.tabs {
-    margin-top:0; margin-bottom: 2em;
-    margin-left: 0;
-  }
+div.modal-dialog {
+  min-width: 700px !important;
+}
 
-  img.preview_upload {width: 150px; margin-right:1em; margin-top:-.75em; padding:0;}
-  .image-upload-wrapper {
-    height: 200px;
-  }
+.ql-snow .ql-formats {
+  margin-right: 8px !important;
+}
 
-  .group {
-      display: flex;
-    }
-    .input-group {
-      margin-right: 5px;
-    }
+.ql-snow .ql-formats button {
+  padding-right: 0px;
+}
 
+#livePreviewTitle,
+#livePreviewAuthor {
+  font-weight: bold;
+  margin-left: .15em;
+  font-family: Garamond, Baskerville, "Baskerville Old Face", "Hoefler Text", "Times New Roman", serif;
+  font-size: 18pt;
+  line-height: 1em;
+  color: rgba(139, 69, 19, .5);
+  text-shadow: 0px 1px 0px rgba(256, 256, 256, .3),
+    0px -1px 0px rgba(0, 0, 0, .2),
+    0px 0px 2px rgba(255, 215, 0, 0.5);
+}
 
+#livePreviewAuthor {
+  font-size: 16pt;
+}
 
+#bookCoverSmall {
+  min-width: 200px;
+  min-height: 300px;
+  border: 1px dashed gray;
+}
+
+/*Tabs control*/
+
+.row.tabs {
+  margin-top: 0;
+  margin-bottom: 2em;
+  margin-left: 0;
+}
+
+img.preview_upload {
+  width: 150px;
+  margin-right: 1em;
+  margin-top: -.75em;
+  padding: 0;
+}
+
+.image-upload-wrapper {
+  height: 200px;
+}
+
+.group {
+  display: flex;
+}
+
+.input-group {
+  margin-right: 5px;
+}
 </style>
