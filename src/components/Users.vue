8e96813f26ed08da5a5d76b8eb1dd0d8cbb5f3c3
anastasiia-perederii <109763363+anastasiia-perederii@users.noreply.github.com>
2023-01-18 13:45:44 +0200

    test

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -178,7 +178,7 @@ export default {
     },
     */
 
-    selectRoles: {
+    selectedRole: {
       get() {
       if (this.user.roles != undefined && this.user.roles != "") {
         return 'Select Roles'
