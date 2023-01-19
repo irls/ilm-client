2c50fa2c80a5152a149b51901f35208b360802a3
anastasiia-perederii <109763363+anastasiia-perederii@users.noreply.github.com>
2023-01-19 22:36:16 +0200

    test

--- src/components/collections/CollectionCoverModal.vue
+++ src/components/collections/CollectionCoverModal.vue
@@ -25,9 +25,9 @@
               <div class="group">
                 <div class="input-group">
                   <span class="input-group-addon"><i class="fa fa-globe"></i></span>
-                  <input id="myURL" type="text" class="form-control" placeholder="URL" v-model="uploadURL" />
+                  <input type="text" class="form-control" placeholder="URL" v-model="uploadURL" />
                 </div>
-                  <button class="btn btn-default" @click="document.getElementById('myURL').value = ''" ><i class="fa fa-trash-o"></i></button>
+                  <button class="btn btn-default" @click="resetInput" ><i class="fa fa-trash-o"></i></button>
                 </div>
                 <br> &nbsp;&nbsp;&nbsp;  or <br><br>
                 <label class='btn btn-default' type="file">
@@ -91,7 +91,8 @@
         },
 
         resetInput () {
-          this.uploadURL = "";
+          this.uploadURL = null;
+          
         },
         
         createImage (file) {
