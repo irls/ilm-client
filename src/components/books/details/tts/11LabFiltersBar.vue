1180c6e2b3a981d8d04b6a9f4c19f1096fba2419
mirashAK <bers_777@ukr.net>
2026-03-17 18:58:39 +0200

    Filters set

--- src/components/books/details/tts/11LabFiltersBar.vue
+++ src/components/books/details/tts/11LabFiltersBar.vue
@@ -14,46 +14,79 @@
     </div>
 
     <!-- Language Dropdown -->
-    <MultiSelect v-if="mapVoiceFilterLanguages.length > 1"
-      v-model="multiVoiceFilters.language"
+    <MultiSelect v-if="mapVoiceFilterLanguages.length > 0"
+      v-model="multiSelectVoiceModel.language"
+      :selectionLimit="1" class="multi-select-language"
       :options="mapVoiceFilterLanguages" optionLabel="caption"
       data-captions="Languages" placeholder="Language"
       display="chip" :showToggleAll="false"
       @change="filterVoiceChange" />
 
     <!-- Accent Dropdown -->
-    <MultiSelect v-if="mapVoiceFilterAccents.length > 1"
-      v-model="multiVoiceFilters.accent"
+    <MultiSelect v-if="lengthVoiceFilterAccents > 0"
+      v-model="multiSelectVoiceModel.accent"
+      class="multi-select-accent"
       :options="mapVoiceFilterAccents" optionLabel="caption"
-      data-captions="Languages" placeholder="Accent"
+      data-captions="Accents" placeholder="Accent"
       display="chip" :showToggleAll="false"
       @change="filterVoiceChange" />
 
-    <!-- Age Dropdown -->
-    <MultiSelect v-if="mapVoiceFilterLibraries.age.length > 1"
-      v-model="multiVoiceFilters.age"
-      :options="mapVoiceFilterLibraries.age" optionLabel="caption"
-      data-captions="Languages" placeholder="Age"
+    <div class="or-divider"><span>or</span></div>
+
+    <!-- Native language Dropdown -->
+    <MultiSelect v-if="mapVoiceFilterLanguages.length > 0"
+      v-model="multiSelectVoiceModel.nativeLanguage"
+      class="multi-select-native-lang"
+      :options="mapVoiceFilterLanguages" optionLabel="caption"
+      data-captions="Native languages" placeholder="Native language"
       display="chip" :showToggleAll="false"
       @change="filterVoiceChange" />
 
     <!-- Gender Dropdown -->
-    <MultiSelect v-if="mapVoiceFilterLibraries.gender.length > 1"
-      v-model="multiVoiceFilters.gender"
+    <MultiSelect v-if="mapVoiceFilterLibraries.gender.length > 0"
+      v-model="multiSelectVoiceModel.gender"
+      class="multi-select-gender"
       :options="mapVoiceFilterLibraries.gender" optionLabel="caption"
-      data-captions="Languages" placeholder="Gender"
+      data-captions="Genders" placeholder="Gender"
+      display="chip" :showToggleAll="false"
+      @change="filterVoiceChange" />
+
+    <!-- Age Dropdown -->
+    <MultiSelect v-if="mapVoiceFilterLibraries.age.length > 0"
+      v-model="multiSelectVoiceModel.age"
+      class="multi-select-age"
+      :options="mapVoiceFilterLibraries.age" optionLabel="caption"
+      data-captions="Ages" placeholder="Age"
+      display="chip" :showToggleAll="false"
+      @change="filterVoiceChange" />
+
+    <!-- HQ Dropdown -->
+    <MultiSelect v-if="mapVoiceFilterHQ.length > 0"
+      v-model="multiSelectVoiceModel.hq"
+      :selectionLimit="1" class="multi-select-hq"
+      :options="mapVoiceFilterHQ" optionLabel="caption"
+      data-captions="HQ" placeholder="HQ"
       display="chip" :showToggleAll="false"
       @change="filterVoiceChange" />
 
     <!-- Notice Dropdown -->
-    <MultiSelect v-if="mapVoiceFilterLibraries.notice.length > 1"
-      v-model="multiVoiceFilters.notice"
+    <MultiSelect v-if="mapVoiceFilterLibraries.notice.length > 0"
+      v-model="multiSelectVoiceModel.notice"
+      class="multi-select-notice"
       :options="mapVoiceFilterLibraries.notice" optionLabel="caption"
-      data-captions="Languages" placeholder="Notice"
+      data-captions="Notices" placeholder="Notice"
       display="chip" :showToggleAll="false"
       @change="filterVoiceChange" />
   </div>
+  <!--<div class="voice-filters-first-row"-->
+  <div class="voice-filters-second-row" ref="voiceFiltersSecondRow">
+    <button class="btn btn-default" v-on:click="cleanFilterVal('voiceFilters.filter'); resetFilters(); filterVoiceChange('filter');">Reset filters</button>
+    <button class="btn btn-primary" v-on:click="applyFilters">Filter</button>
+  </div>
+  <!--<div class="voice-filters-second-row"-->
+
 </div>
+<!--<div class="voice-filters-main-wrapper">-->
 </template>
 
 <script>
@@ -90,12 +123,12 @@ export default {
     },
     watch: {},
     mounted() {
-      console.log(`${__filename.slice(-30)}:mounted:elevenLabsVoicesFilters: `);
+      console.log(`${__filename.slice(-30)}:mounted:elevenLabsVoicesFilters: `, this.mapVoiceFilterHQ.length);
     },
     updated() {},
     methods: {
       filterVoiceChange (key, $event) {
-        const newFilters = Object.entries(this.multiVoiceFilters).reduce((acc, [key, val])=>{
+        const newFilters = Object.entries(this.multiSelectVoiceModel).reduce((acc, [key, val])=>{
           acc[key] = val.map((el)=>el.value);
           return acc;
         }, {});
@@ -104,7 +137,7 @@ export default {
           newFilters.filter = $event ? $event.target.value : '';
         }
         this.$store.commit('elevenLabsVoicesFilters/set_voiceFilters', newFilters);
-
+        console.log(`elevenLabsVoicesFilters/set_voiceFilters::: `, newFilters);
 
         this.changeFilterVisual();
       },
@@ -113,11 +146,19 @@ export default {
         this.filterVoiceChange(key, $event)
       }, 300),
 
+      applyFilters() {
+
+      },
+
       cleanFilterVal (key) {
         // To unlink chain between the input value and the model because of debounce
         if (this.$refs[key]) {
           this.$refs[key].value = '';
         }
+      },
+
+      resetFilters () {
+        this.$store.commit('elevenLabsVoicesFilters/set_resetVoiceFilters');
       },
 
       changeFilterVisual() {
@@ -155,11 +196,13 @@ export default {
     },
     computed: {
       ...mapGetters({
-        voiceFilters:            'elevenLabsVoicesFilters/voiceFilters',
-        multiVoiceFilters:       'elevenLabsVoicesFilters/multiVoiceFilters',
-        mapVoiceFilterLanguages: 'elevenLabsVoicesFilters/mapVoiceFilterLanguages',
-        mapVoiceFilterAccents:   'elevenLabsVoicesFilters/mapVoiceFilterAccents',
-        mapVoiceFilterLibraries: 'elevenLabsVoicesFilters/mapVoiceFilterLibraries',
+        voiceFilters:             'elevenLabsVoicesFilters/voiceFilters',
+        multiSelectVoiceModel:    'elevenLabsVoicesFilters/multiSelectVoiceModel',
+        mapVoiceFilterLanguages:  'elevenLabsVoicesFilters/mapVoiceFilterLanguages',
+        mapVoiceFilterAccents:    'elevenLabsVoicesFilters/mapVoiceFilterAccents',
+        lengthVoiceFilterAccents: 'elevenLabsVoicesFilters/lengthVoiceFilterAccents',
+        mapVoiceFilterLibraries:  'elevenLabsVoicesFilters/mapVoiceFilterLibraries',
+        mapVoiceFilterHQ:         'elevenLabsVoicesFilters/mapVoiceFilterHQ',
       }),
     },
     components: {
@@ -181,12 +224,110 @@ export default {
     .voice-filters-text-filter {
       width: 450px;
       margin-bottom: 10px;
+      margin-right: 10px;
+
+      input.form-control {
+        display: inline;
+        box-shadow: none;
+        padding-top: 4px;
+        padding-bottom: 4px;
+        height: 32px;
+      }
+
+      input.form-control.voice-filter.filled {
+        padding-right: 30px;
+      }
+
+      .btn-inside {
+        margin-left: -26px;
+        margin-top: 5px;
+        z-index: 999;
+        position: absolute;
+      }
     }
 
     .p-component.p-multiselect {
       margin-bottom: 10px;
+      margin-right: 10px;
+
+      min-width: 14rem;
+      border-radius: 4px;
+      border: 1px solid #ccc;
+      box-shadow: none;
+
+      &.multi-select-language {
+        min-width: 10rem;
+      }
+      &.multi-select-accent {
+
+      }
+      &.multi-select-gender {
+        min-width: 10rem;
+      }
+      &.multi-select-age {
+        min-width: 8rem;
+      }
+      &.multi-select-hq {
+        min-width: 5rem;
+      }
+      &.multi-select-notice {
+        min-width: 8rem;
+      }
+    }
+
+    .or-divider {
+      margin-bottom: 10px;
+      margin-right: 10px;
+      padding-top: 4px;
+    }
+  }
+
+  .voice-filters-second-row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+
+    button.btn {
+      margin-left: 15px;
     }
   }
 }
+</style>
 
+<style lang="less">
+.voice-filters-main-wrapper {
+  .voice-filters-first-row {
+    .p-component.p-multiselect.p-multiselect-chip {
+      .p-multiselect-token {
+        border-radius: 4px;
+        padding: 0.25rem 0.5rem;
+        margin-right: 0.5rem;
+        background: #edebe9;
+        color: #323130;
+
+        .p-multiselect-token-icon {
+          display: none;
+          margin-left: 0.5rem;
+        }
+      }
+    }
+  }
+  .p-component.p-multiselect {
+    .p-multiselect-label.p-placeholder {
+      color: #888;
+    }
+    &.multi-select-language,
+    &.multi-select-gender,
+    &.multi-select-age {
+      .p-multiselect-label.p-placeholder {
+        &:before {
+          content: "*";
+          color: red;
+          vertical-align: middle;
+        }
+      }
+    }
+  }
+}
 </style>
--- src/store/modules/11LabVoiceFilters.js
+++ src/store/modules/11LabVoiceFilters.js
@@ -11,29 +11,48 @@ export default {
   state: {
     fltrChangeTrigger: false,
 
-    voiceFilters: {
-      filter: '',
-      multiProjectTag: [],
-      importStatus: [],
-      language: [],
-      jobStatus: ['active'],
-      booksIds: [],
-    },
-
     defaultVoiceFilters: {
+      page: 0,
       filter: '',
-      importStatus: [],
-      multiProjectTag: [],
       language: [],
-      jobStatus: ['active'],
-      booksIds: [],
+      accent: [],
+      nativeLanguage: [],
+      hq: [],
+      age: [],
+      gender: [],
+      notice: []
     },
 
-    multiVoiceFilters: {
+    voiceFilters: {
+      page: 0,
+      filter: '',
       language: [],
-      jobStatus: [{caption: 'Active', value: 'active'}],
-      importStatus: [],
-      multiProjectTag: []
+      accent: [],
+      nativeLanguage: [],
+      hq: [],
+      age: [],
+      gender: [],
+      notice: []
+    },
+
+    defaultMultiSelectVoiceModel: {
+      language: [],
+      accent: [],
+      nativeLanguage: [],
+      hq: [],
+      age: [],
+      gender: [],
+      notice: []
+    },
+
+    multiSelectVoiceModel: {
+      language: [],
+      accent: [],
+      nativeLanguage: [],
+      hq: [],
+      age: [],
+      gender: [],
+      notice: []
     },
 
     // mapFilterJobStatus: [
@@ -41,15 +60,6 @@ export default {
     //   {caption: 'Archived',  value: 'archived'},
     //   {caption: 'Completed', value: 'completed'},
     //   {caption: 'Suspended', value: 'suspended'}
-    // ],
-    //
-    // mapFilterImportStatus: [
-    //   {caption: 'No content',   value: 'staging_empty'},
-    //   {caption: 'Text Cleanup', value: 'staging'},
-    //   {caption: 'Narration',    value: 'narrating'},
-    //   {caption: 'Proofreading', value: 'proofing'},
-    //   {caption: 'Mastering',    value: 'mastering'},
-    //   {caption: 'Done',         value: 'completed'}
     // ],
 
     languageFilterList: {
@@ -65,19 +75,16 @@ export default {
       age: [],
       gender: [],
       notice: []
-    }
+    },
+    hqFilterList: {
+      loaded: true,
+      list: [{caption: 'HQ', value: 'hq'}]
+    },
   },
   getters: {
     fltrChangeTrigger:           state => state.fltrChangeTrigger,
     voiceFilters:                state => state.voiceFilters,
-    multiVoiceFilters:           state => state.multiVoiceFilters,
-
-    // mapFilterJobStatus: (state, getters, rootState, rootGetters) => {
-    //   if (state.jobStatusFilterList.loaded) {
-    //     return state.jobStatusFilterList.list;
-    //   }
-    //   return [];
-    // },
+    multiSelectVoiceModel:       state => state.multiSelectVoiceModel,
 
     mapVoiceFilterLanguages: (state, getters, rootState, rootGetters) => {
       if (state.languageFilterList.loaded) {
@@ -87,10 +94,19 @@ export default {
     },
 
     mapVoiceFilterAccents: (state, getters, rootState, rootGetters) => {
-      if (state.accentFilterList.loaded) {
-        return state.accentFilterList.list;
+      if (state.voiceFilters.language.length && state.accentFilterList.loaded) {
+        return Object.entries(state.accentFilterList.list).reduce((acc, [key, _val])=>{
+          if (state.voiceFilters.language.indexOf(key) > -1) {
+            acc = [...acc, ..._val];
+          }
+          return acc;
+        }, []).sort((a, b) => a.order - b.order);
       }
       return [];
+    },
+
+    lengthVoiceFilterAccents: (state, getters, rootState, rootGetters) => {
+      return Object.keys(state.accentFilterList).length;
     },
 
     mapVoiceFilterLibraries: (state, getters, rootState, rootGetters) => {
@@ -100,6 +116,13 @@ export default {
           gender: state.librariesFilterObj.gender,
           notice: state.librariesFilterObj.notice
         }
+      }
+      return [];
+    },
+
+    mapVoiceFilterHQ: (state, getters, rootState, rootGetters) => {
+      if (state.hqFilterList.loaded) {
+        return state.hqFilterList.list;
       }
       return [];
     },
@@ -115,21 +138,23 @@ export default {
     // },
   },
   mutations: {
-    set_multiVoiceFilters(state, payload) {
-      state.multiVoiceFilters = payload;
+    set_multiSelectVoiceModel(state, payload) {
+      state.multiSelectVoiceModel = payload;
     },
 
-    set_voiceFilters (state, obj) { // replace any property of bookFilters
+    set_voiceFilters (state, obj) { // replace any property of voiceFilters
       for (const prop in obj) {
-        if (['filter', 'multiProjectTag', 'importStatus', 'language', 'jobStatus', 'page'].indexOf(prop) > -1) {
+        const filtersLabels = Object.keys(state.defaultVoiceFilters);
+        if (filtersLabels.indexOf(prop) > -1) {
           state.voiceFilters[prop] = obj[prop];
         }
       }
       state.fltrChangeTrigger = !state.fltrChangeTrigger;
     },
 
-    set_clearVoiceFilters (state) {
-      state.booksFilters = Object.assign({}, state.defaultBooksFilters);
+    set_resetVoiceFilters (state) {
+      state.voiceFilters = Object.assign({}, state.defaultVoiceFilters);
+      state.multiSelectVoiceModel = Object.assign({}, state.defaultMultiSelectVoiceModel);
       state.fltrChangeTrigger = !state.fltrChangeTrigger;
     },
 
@@ -138,20 +163,39 @@ export default {
     },
 
     set_languageFilterList (state, list) {
-      state.languageFilterList.list = list.map((lang)=>({caption: lang.label ? lang.label : lang.name, value: lang.name}));
+      state.languageFilterList.list = list.map((lang)=>({
+        caption: lang.label ? lang.label : lang.name,
+        value: lang.name
+      }));
       state.languageFilterList.loaded = true;
     },
 
     set_accentFilterList (state, list) {
-      console.log(`set_accentFilterList::: `, list);
-      // state.accentFilterList.list = list.map((lang)=>({caption: lang.label ? lang.label : lang.name, value: lang.name}));
-      // state.accentFilterList.loaded = true;
+      state.accentFilterList.list = Object.keys(list).reduce((acc, key)=>{
+        acc[key] = list[key].map((_val)=>({
+          caption: _val.label ? _val.label : _val.name,
+          value: _val.name,
+          order: _val.order,
+        }));
+        return acc;
+      }, {})
+
+      state.accentFilterList.loaded = true;
     },
 
     set_librariesFilterList (state, obj) {
-      state.librariesFilterObj.age = obj?.age?.map((_val)=>({caption: _val.label ? _val.label : _val.name, value: _val.name}));
-      state.librariesFilterObj.gender = obj?.gender?.map((_val)=>({caption: _val.label ? _val.label : _val.name, value: _val.name}));
-      state.librariesFilterObj.notice = obj?.notice?.map((_val)=>({caption: _val.label ? _val.label : _val.name, value: _val.name}));
+      state.librariesFilterObj.age = obj?.age?.map((_val)=>({
+        caption: _val.label ? _val.label : _val.name,
+        value: _val.name
+      }));
+      state.librariesFilterObj.gender = obj?.gender?.map((_val)=>({
+        caption: _val.label ? _val.label : _val.name,
+        value: _val.name
+      }));
+      state.librariesFilterObj.notice = obj?.notice?.map((_val)=>({
+        caption: _val.label ? _val.label : _val.name,
+        value: _val.name
+      }));
       state.librariesFilterObj.loaded = true;
     }
   },
